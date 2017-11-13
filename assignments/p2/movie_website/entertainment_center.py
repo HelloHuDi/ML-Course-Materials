2dc301e2968a1edcb3b37ad826d4a509f65904a1
hudi <hellohudi@163.com>
2017-11-13 08:57:08 +0800

    change p2

--- assignments/p2/movie_website/entertainment_center.py
+++ assignments/p2/movie_website/entertainment_center.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
 import media
 import fresh_tomatoes
 
--- assignments/p2/movie_website/media.py
+++ assignments/p2/movie_website/media.py
@@ -1,5 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
 import webbrowser
-
 
 class Movie():
     def __init__(self, movie_title, movie_storyline,
